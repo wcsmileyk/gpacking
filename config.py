c3e38bb12ddcbd34a3ed0298f0ad17a00702b31d
Wes Smiley <wes.smiley@phishme.com>
2017-07-08 00:44:16 -0400

    Added migrations

--- config.py
+++ config.py
@@ -22,8 +22,7 @@ class DevelopmentConfig(Config):
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-                              'mysql+pymysql://gpacking:gpacking@localhost/gpacking'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://gpacking:gpacking@localhost/gpacking'
 
 
 class TestingConfig(Config):
